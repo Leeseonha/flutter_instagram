e13b02c59fbd4fe621e2ae60a2941ece366d805a
Leeseonha <jmtkdsh@gmail.com>
2022-08-25 17:44:08 +0900

    메인페이지 하단 버튼 이미지 추가

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/src/components/image_data.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return WillPopScope(
+        child: Scaffold(
+          appBar: AppBar(),
+          body: Container(),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: 0,
+            elevation: 0, // 0으로 설정하면 버튼 하단 부분 위에 선이 사라짐
+            onTap: (value) {},
+            items: [
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.homeOff),
+                activeIcon: ImageData(IconsPath.homeOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.searchOff),
+                activeIcon: ImageData(IconsPath.searchOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.uploadIcon),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.activeOff),
+                activeIcon: ImageData(IconsPath.activeOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  width: 30,
+                  height: 30,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey,
+                  ),
+                ),
+                label: 'home',
+              ),
+            ],
+          ),
+        ),
+        onWillPop: () async {
+          return false;
+        });
   }
 }
